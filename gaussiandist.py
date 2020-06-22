11bb6f61b5e90ebbd32195e23e57adf1a91dc71a
jungsnn <jjung9310@gmail.com>
2020-06-22 01:06:32 -0400

    created a separate parent class from Gaussian module

--- gaussiandist.py
+++ gaussiandist.py
@@ -1,23 +1,11 @@
 import math
 import matplotlib.pyplot as plt
+from generaldistribution import Distribution
 
-class Gaussian():
-    """ Gaussian distribution class for calculating and
-    visualizing a Gaussian distribution.
+class Gaussian(Distribution):
 
-    Attributes:
-        mean (float) representing the mean value of the distribution
-        stdev (float) representing the standard deviation of the distribution
-        data_list (list of floats) a list of floats extracted from the data file
-
-    """
-    def __init__(self, mu = 0, sigma = 1):
-
-        self.mean = mu
-        self.stdev = sigma
-        self.data = []
-
-
+    def __init__(self, mu=0, sigma=1):
+        Distribution.__init__(self, mu, sigma)
 
     def calculate_mean(self):
 
