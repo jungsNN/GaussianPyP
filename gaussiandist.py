15731e7ff379b57ea1e09f3d4e157e3f8151c6a8
jungsnn <jjung9310@gmail.com>
2020-06-22 01:16:02 -0400

    edit import file call

--- gaussiandist.py
+++ gaussiandist.py
@@ -1,9 +1,17 @@
 import math
 import matplotlib.pyplot as plt
-from generaldistribution import Distribution
+from .generaldistribution import Distribution
 
 class Gaussian(Distribution):
+    """ Gaussian distribution class for calculating and
+    visualizing a Gaussian distribution.
 
+    Attributes:
+        mean (float) representing the mean value of the distribution
+        stdev (float) representing the standard deviation of the distribution
+        data_list (list of floats) a list of floats extracted from the data file
+
+    """
     def __init__(self, mu=0, sigma=1):
         Distribution.__init__(self, mu, sigma)
 
